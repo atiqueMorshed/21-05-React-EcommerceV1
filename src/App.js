46480f0f33293203306af83955f88bc6fcfe3bc7
Atique Morshed <iamsamix17@gmail.com>
2021-05-15 03:45:02 +0600

    Updated Dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import {Route} from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS</h1>
+  </div>
+)
+
 function App() {
   return (
     <div>
-      <HomePage/>
+      <switch> {/* Goes char by char and as soon as a match is found, breaks and loads that component */}
+        <Route exact path='/' component={HomePage} /> {/* exact[true/false] if true, only renders component which strictly matches the path */}
+        <Route path='/hats' component={HatsPage} />
+      </switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import {BrowserRouter} from 'react-router-dom';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter> {/* This component give the application inside it's scope [Here <App/>] all the routing functionality  */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
