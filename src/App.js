4581c058a53cea602fcdf218eaa91244a775f917
Atique Morshed Sami <iamsamix17gmail.com>
2021-05-17 08:35:21 +0600

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import {Route, Switch} from 'react-router-dom';
 
-
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch> {/* Goes char by char and as soon as a match is found, breaks and loads that component */}
         <Route exact path='/' component={HomePage} /> {/* exact[true/false] if true, only renders component which strictly matches the path */}
         <Route path='/shop' component={ShopPage} />
