f81e9bba65005fd198951f34b542f37268742dd6
Atique Morshed Sami <iamsamix17gmail.com>
2021-05-18 11:16:01 +0600

    Added Components: SignIn, FormInput, CustomButton

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 import {Route, Switch} from 'react-router-dom';
 
 import Header from './components/header/header.component';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <Switch> {/* Goes char by char and as soon as a match is found, breaks and loads that component */}
         <Route exact path='/' component={HomePage} /> {/* exact[true/false] if true, only renders component which strictly matches the path */}
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './form-input.styles.scss';
+
+const FormInput = ({handleChange, label, ...otherFormInputProps}) => (
+    <div className='group'>
+        <input className='form-input' onChange={handleChange} {...otherFormInputProps} />
+        { label ? (<label className={ `${otherFormInputProps.value ? 'shrink' : '' } form-input-label`}> {label} </label>) : null }
+    </div>
+)
+
+export default FormInput;
