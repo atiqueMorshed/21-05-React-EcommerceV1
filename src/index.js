9147e168dddb9b7c2fb82027ee5f8949d4847a0b
Atique Morshed Sami <iamsamix17@gmail.com>
2021-06-03 04:07:35 +0600

    +Local Storage with redux-persist for cart storage

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 
 import {Provider} from 'react-redux';
-import store from './redux/store';
+import {store, persistor} from './redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
 
 import {BrowserRouter} from 'react-router-dom';
 
@@ -12,7 +13,9 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter> {/* This component give the application inside it's scope [Here <App/>] all the routing functionality  */}
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
