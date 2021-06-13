cfa7d5202cc44c7b4e9d81f2d06390ceb754e7ee
Atique Morshed Sami <iamsamix17@gmail.com>
2021-06-14 00:07:20 +0600

    +Stripe backend

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({price}) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51IytWXEgVRAV5lBT3kmAmx8r8VHfW4kwyaFzXotdaGrX3ThjrWSkPziDSvkLPmyOPumENz5ljT8NdAPNGrakkVyq00QWhqBZ9V';
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment Successful!');
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response =>{
+            alert("Payment Successful.");
+        }).catch(error => {
+            console.log('Payment Error: ', JSON.parse(error));
+            alert("ERROR: PLEASE USE THE SPECIFIED CARD INFORMAITON!");
+        });
     };
 
     return (
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -21,4 +21,4 @@ sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 
-export default {store, persistor};
+// export default {store, persistor};
