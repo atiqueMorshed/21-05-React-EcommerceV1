e5a94cef9cb6116c606349db63af0b2061ee9f52
Atique Morshed Sami <iamsamix17@gmail.com>
2021-06-15 13:35:16 +0600

    removed redux

--- src/index.js
+++ src/index.js
@@ -2,28 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 
-
-import {Provider} from 'react-redux';
-import {store, persistor} from './redux/store';
-import {PersistGate} from 'redux-persist/integration/react';
-
 import CartProvider from './providers/cart/cart.provider';
 
 import './index.css';
 import App from './App';
 
-
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
       <CartProvider>
         <BrowserRouter> {/* This component give the application inside it's scope [Here <App/>] all the routing functionality  */}
-          <PersistGate persistor={persistor}>
-            <App />
-          </PersistGate>
+          <App />
         </BrowserRouter>
       </CartProvider>
-    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/redux/cart/cart.types.js
+++
@@ -1,8 +0,0 @@
-const cartActionTypes = {
-    TOGGLE_CART_HIDDEN:'TOGGLE_CART_HIDDEN',
-    ADD_ITEM:'ADD_ITEM',
-    REMOVE_ITEM:'REMOVE_ITEM',
-    REMOVE_THE_WHOLE_ITEM:'REMOVE_THE_WHOLE_ITEM'
-};
-
-export default cartActionTypes;
